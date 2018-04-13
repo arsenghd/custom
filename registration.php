4c4db9b7ca0e1140945c8e4acb97576dd098d948
Vahe <abelyan.vahe@gmail.com>
2018-04-13 18:20:08 +0400

    3 commit

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
     Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Magento_SampleNewPage',
+    'Arsen_SampleNewPage',
     __DIR__
 );
